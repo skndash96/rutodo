e32bc54e3eb1c5bfe1f13189eb80c80d6bc9eca9
Dash Skndash <78152517+skndash96@users.noreply.github.com>
2023-02-11 15:11:08 +0000

    .

--- target/debug/deps/my_project-b8d67f76aa7ad6a1.d
+++
@@ -1,6 +0,0 @@
-/home/runner/rusty-todo/target/debug/deps/my_project-b8d67f76aa7ad6a1: src/main.rs src/cmds.rs
-
-/home/runner/rusty-todo/target/debug/deps/my_project-b8d67f76aa7ad6a1.d: src/main.rs src/cmds.rs
-
-src/main.rs:
-src/cmds.rs:
--- target/debug/my-project.d
+++
@@ -1,1 +0,0 @@
-/home/runner/rusty-todo/target/debug/my-project: /home/runner/rusty-todo/src/cmds.rs /home/runner/rusty-todo/src/main.rs
